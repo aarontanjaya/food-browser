65e72b19c6f8f4203bcb2cbe7a4f7861a6411857
aarontanjaya <aaront.tjy@gmail.com>
2025-02-08 21:52:07 +0700

    feat: add husky pre commits

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,33 @@
 import js from '@eslint/js';
+import airbnb from 'eslint-config-airbnb';
+import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended, 'airbnb', 'airbnb/hooks', 'airbnb-typescript'],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parser: tseslint.parser,
-      parserOptions: {
-        project: './tsconfig.json',
-      },
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+export default tseslint.config({
+  ignores: ['dist'],
+  extends: [js.configs.recommended, tseslint.configs.recommended],
+  files: ['src/**/*.{ts,tsx}'],
+  languageOptions: {
+    ecmaVersion: 2020,
+    globals: globals.browser,
+    parser: tseslint.parser,
+    parserOptions: {
+      project: ['./tsconfig.json', './tsconfig.node.json', './tsconfig.app.json'],
     },
   },
-);
+  plugins: {
+    react,
+    'react-hooks': reactHooks,
+    'react-refresh': reactRefresh,
+  },
+  rules: {
+    ...airbnb.rules,
+    ...reactHooks.configs.recommended.rules,
+    ...react.configs.recommended.rules,
+    ...react.configs['jsx-runtime'].rules,
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+  },
+});

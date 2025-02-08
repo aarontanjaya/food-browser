91e9c46cdb6bfe07e5f79ed7db3a9faddf297c60
aarontanjaya <aaront.tjy@gmail.com>
2025-02-08 22:23:17 +0700

    fix: fix eslint configuration for eslint-plugin-react

--- eslint.config.js
+++ eslint.config.js
@@ -6,28 +6,35 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config({
-  ignores: ['dist'],
-  extends: [js.configs.recommended, tseslint.configs.recommended],
-  files: ['src/**/*.{ts,tsx}'],
-  languageOptions: {
-    ecmaVersion: 2020,
-    globals: globals.browser,
-    parser: tseslint.parser,
-    parserOptions: {
-      project: ['./tsconfig.json', './tsconfig.node.json', './tsconfig.app.json'],
+export default tseslint.config(
+  { ignores: ['dist/*', 'public/*'] },
+  {
+    extends: [js.configs.recommended, tseslint.configs.recommended],
+    files: ['src/**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parser: tseslint.parser,
+      parserOptions: {
+        project: ['./tsconfig.json', './tsconfig.node.json', './tsconfig.app.json'],
+      },
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      ...airbnb.rules,
+      ...reactHooks.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
   },
-  plugins: {
-    react,
-    'react-hooks': reactHooks,
-    'react-refresh': reactRefresh,
-  },
-  rules: {
-    ...airbnb.rules,
-    ...reactHooks.configs.recommended.rules,
-    ...react.configs.recommended.rules,
-    ...react.configs['jsx-runtime'].rules,
-    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-  },
-});
+);

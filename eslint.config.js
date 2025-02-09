97f54405c22d919c88838c4d408af1284ef92819
aarontanjaya <aaront.tjy@gmail.com>
2025-02-09 13:17:38 +0700

    feat: add urlcat and adjust yarn and tsconfig to support urlcat

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist/*', 'public/*'] },
+  { ignores: ['dist/*', 'public/*', 'coverage', '.yarn'] },
   {
     extends: [js.configs.recommended, tseslint.configs.recommended],
     files: ['src/**/*.{ts,tsx}'],
